c56d96f2e877e29f07615a085503365ae417568d
Alex <alexandrina.mehandzhiyska1@gmail.com>
2022-04-02 20:19:03 +0300

    Add auth header

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 
+import Home from './components/Home/Home';
 import Layout from './components/layout/Layout';
 import Login from './components/Login/Login';
 import MonthlyReport from './components/MonthlyReport/MonthlyReport';
 import Register from './components/Register/Register';
 
 function App() {
+  const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
+
   return (
     <Layout>
       <Routes>
-        <Route exact path="/" element={<Navigate to="/login" />}/>
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />}/>
         <Route path="/monthly-report" element={<MonthlyReport />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,18 +1,32 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 const Header = () => {
     const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
-    console.log(user);
+    const navigate = useNavigate();
+    
+    const logout = () => {
+        localStorage.removeItem('user');
+        navigate('/login');
+    }
 
     return (
         <header className="flex justify-between items-center px-16">
             <h1 className="text-xl px-5">DaDay</h1>
 
             <nav>
-                <ul className="flex py-4">
-                    <li className="px-5"><Link to='/register'>Register</Link></li>
-                    <li className="px-5"><Link to='/login'>Login</Link></li>
-                </ul>
+                {!user && 
+                    <ul className="flex py-4">
+                        <li className="px-5"><Link to='/register'>Register</Link></li>
+                        <li className="px-5"><Link to='/login'>Login</Link></li>
+                    </ul>
+                }
+
+                {user && 
+                    <ul className="flex py-4">
+                        <li className="px-5" onClick={logout}>Logout</li>
+                    </ul>
+                }
             </nav>
         </header>
     );
