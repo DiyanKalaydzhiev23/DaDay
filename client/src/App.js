0097ecc178a76080e74c97f6c9c6f1e469f18ced
Alex <alexandrina.mehandzhiyska1@gmail.com>
2022-04-02 10:46:59 +0300

    Add report chart

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
 import Home from './components/Home/Home';
+import ReportChart from './components/MonthlyReport/ReportChart';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/chart" element={<ReportChart />} />
       </Routes>
     </Layout>
   );
