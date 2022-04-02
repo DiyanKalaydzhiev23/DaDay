4af68892303e62f110958e6da461dd3c4ec7b902
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-04-02 20:40:22 +0300

    Update views.py

--- server/da_day/views.py
+++ server/da_day/views.py
@@ -29,7 +29,7 @@ class NoteCreateView(views.APIView):
 
     def get(self, request, pk):
         question = random.choice(self.queryset.all()).__str__()
-
+        print(request.headers.get('Authorization'), Token.objects.get_or_create(user=request.user.id)[0])
         if request.headers.get('Authorization') == Token.objects.get_or_create(user=request.user.id)[0]:
             return Response({'question': question}, status=status.HTTP_200_OK)
 
