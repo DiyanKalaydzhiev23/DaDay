34ce4663345ff9e422b1ae4efa1dcc65a936abe0
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-04-02 13:56:47 +0300

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -116,8 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:3000'
-    'http://localhost:3000',
+    '127.0.0.1:3000'
+    'localhost:3000',
 )
 
 # Internationalization
