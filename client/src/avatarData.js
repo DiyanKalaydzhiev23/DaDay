378450893a5cac0e60eccd37dbb4b912322d5f2f
Alexandrina Mehandzhiyska <alexandrina.mehandzhiyska1@gmail.com>
2022-04-05 19:06:33 +0300

    Add duck images:

---
+++ client/src/avatarData.js
@@ -0,0 +1,24 @@
+// Avatars:
+// 1: cat
+// 2: duck
+
+export const avatarData = {
+    1: [
+        'https://res.cloudinary.com/drinka/image/upload/v1648977221/da-day/cat/cat-main_jmglcl.png',
+        'https://res.cloudinary.com/drinka/image/upload/v1649171722/da-day/cat/cat-animation_kymdwl.gif',
+        'https://res.cloudinary.com/drinka/image/upload/v1649171696/da-day/cat/cat-1_bbjeec.png',
+        'https://res.cloudinary.com/drinka/image/upload/v1649171696/da-day/cat/cat-2_z8hknq.png',
+        'https://res.cloudinary.com/drinka/image/upload/v1649171696/da-day/cat/cat-3_bxkrk7.png',
+        'https://res.cloudinary.com/drinka/image/upload/v1649171696/da-day/cat/cat-4_cxvbr2.png',
+        'https://res.cloudinary.com/drinka/image/upload/v1649171697/da-day/cat/cat-5_ejgtqz.png'
+    ],
+    2: [
+        'https://res.cloudinary.com/drinka/image/upload/v1648973984/da-day/duck/duck-main_nz3pab.png',
+        'https://res.cloudinary.com/drinka/image/upload/v1649173474/da-day/duck/duck-animation_vtytdh.gif',
+        'https://res.cloudinary.com/drinka/image/upload/v1649173468/da-day/duck/duck-1_oxo7p9.png',
+        'https://res.cloudinary.com/drinka/image/upload/v1649173468/da-day/duck/duck-2_qrmprr.png',
+        'https://res.cloudinary.com/drinka/image/upload/v1649173468/da-day/duck/duck-3_mtncee.png',
+        'https://res.cloudinary.com/drinka/image/upload/v1649173468/da-day/duck/duck-4_rbw6mh.png',
+        'https://res.cloudinary.com/drinka/image/upload/v1649173468/da-day/duck/duck-5_tysnkv.png'
+    ]
+};
