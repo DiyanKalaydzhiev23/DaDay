5c4d560c34ac309ca8ffbfc881e806d6390dc8ae
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-04-08 13:10:00 +0300

    fixed sending emails

--- server/auth_app/views.py
+++ server/auth_app/views.py
@@ -22,7 +22,7 @@ class UserCreate(generics.CreateAPIView):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         token, created = Token.objects.get_or_create(user=serializer.instance)
-        my_mail(request, request.data.get('profile.parent_email'))
+        my_mail(request, request.data['profile']['parent_email'])
         user = UserModel.objects.get(username=serializer.data['username'])
         profile = Profile.objects.get(user_id=user.id)
         return Response({'token': token.key, 'user_id': user.id, 'username': user.username, 'avatar': profile.avatar},
